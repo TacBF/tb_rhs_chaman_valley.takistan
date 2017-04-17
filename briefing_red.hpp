f4ef8fae27daaaa65d6e33ac71b5f804881b3811
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:37:34 +0200

    tb_rhs_chaman_valley.takistan mission added

---
+++ briefing_red.hpp
@@ -0,0 +1,13 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Replace all ampersands/greater than/less than symbols with &amp; or &gt; or &lt;
+<titleFont>Deployment Options:</font>
+<*> Get to the AO by: spawning at the FireBase or by going from the main base by Car or APCs.
+
+<titleFont>Attack Strategies:</font>
+<*> Utilize your MW Supplies.
+<*> Establish SRPs quickly.
+<*> No FOs.
+<*> Utilize APC superiority.
+
+
+<titleFont>Defence Strategies:</font>
+<*> Ensure you setup SRPs before advancing to far.
